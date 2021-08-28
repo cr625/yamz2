0560058e42618d8fe09e48557a40a8210dccb9c1
cr625 <cr625@drexel.edu>
2021-08-28 17:34:21 -0400

    added test config

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from instance.config import SECRET_KEY
 import os
 
 from flask import Flask, render_template
@@ -16,43 +17,27 @@ migrate = Migrate()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-def create_app(test_config=None):
+def create_app(test_config="test_config.py"):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY="ASUFFICIENTLYCOMPLECATEDKEYTHATSHOULDBESTOREDINANENVIRONMENTVARIABLE",
-        BOOTSTRAP_BTN_STYLE="dark",
-        SQLALCHEMY_DATABASE_URI="postgresql://postgres:PASS@localhost/yamz",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        YAMZ_ADMIN_EMAIL="christopher.b.rauch@gmail.com",
-    )
 
-    from . import models
-
-    # testing setup
-    #    app.config["SECRET_KEY"] = "hard to guess string"
-    #    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-    #        basedir, "data.sqlite"
-    #    )
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-    login_manager.init_app(app)
-    bootstrap.init_app(app)
-    migrate.init_app(app, db)
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.update(test_config)
-
-    # ensure the instance folder exists
+    # set common config values
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    # if not testing, config is loaded from config.py in the instance folder
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        # whichever config file name you pass in also has to be in the instance folder
+        app.config.from_pyfile(test_config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    bootstrap.init_app(app)
+    migrate.init_app(app, db)
 
     # apply the blueprints to the app
     from .main import main as main_blueprint
@@ -67,6 +52,7 @@ def create_app(test_config=None):
 
     app.register_blueprint(term_blueprint, url_prefix="/term")
 
+    # register command line functions
     @app.cli.command()
     def test():
         """Run the unit tests."""
