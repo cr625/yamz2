7b9b213c3ec4dea8f8b778c2ea04b9214cb496ad
cr625 <cr625@drexel.edu>
2021-08-29 19:48:41 -0400

    added Task model

--- app/graph/views.py
+++ app/graph/views.py
@@ -1,31 +1,10 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, url_for, request, session, redirect, flash
 from . import graph
-from yamz import celery
 import random, time
+from flask_mail import Message
 
 
-@celery.task(bind=True)
-def long_task(self):
-    """Background task that runs a long function with progress reports."""
-    verb = ["Starting up", "Booting", "Repairing", "Loading", "Checking"]
-    adjective = ["master", "radiant", "silent", "harmonic", "fast"]
-    noun = ["solar array", "particle reshaper", "cosmic ray", "orbiter", "bit"]
-    message = ""
-    total = random.randint(10, 50)
-    for i in range(total):
-        if not message or random.random() < 0.25:
-            message = "{0} {1} {2}...".format(
-                random.choice(verb), random.choice(adjective), random.choice(noun)
-            )
-        self.update_state(
-            state="PROGRESS", meta={"current": i, "total": total, "status": message}
-        )
-        time.sleep(1)
-    return {"current": 100, "total": 100, "status": "Task completed!", "result": 42}
-
-
-@graph.route("/import")
-def longtask():
-    task = long_task.apply_async()
-    return "it worked?"
-    # return jsonify({}), 202, {"Location": url_for("taskstatus", task_id=task.id)}
+@graph.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("/graph/index.html")
