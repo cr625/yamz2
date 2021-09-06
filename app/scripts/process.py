9be07da692bb7cc58c8d850da305353ef2a009c2
cr625 <cr625@drexel.edu>
2021-09-06 17:06:51 -0400

    added predicate and object import

--- app/scripts/process.py
+++ app/scripts/process.py
@@ -1,12 +1,28 @@
 #!./venv/bin/python3
 
-from rdflib import Graph, URIRef, Literal, Namespace, RDF, RDFS, XSD
+from rdflib import Graph, URIRef, Literal, Namespace, RDF, RDFS, XSD, term
+from rdflib.namespace import SKOS, DC, DCTERMS
 import pprint
 
 g = Graph()
 g.parse("./graph/uploads/dublin_core_elements.nt")
 
-print(len(g))
+
+def check_char(x):
+    i = -1
+    if "#" in x:
+        i = x.rfind("#")
+    elif "/" in x and not x.endswith("/"):
+        i = x.rfind("/")
+    return x[i + 1 :].strip()
+
 
 for s, p, o in g:
-    print("{}".format(s))
+    s = check_char(s)
+    print("subject: {}".format(s))
+
+    p = check_char(p)
+    print("predicate: {}".format(p))
+
+    o = check_char(o)
+    print("object: {}\n".format(o))
