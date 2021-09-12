5e900e1d895b4b5845502598961c445e7637c41f
cr625 <cr625@drexel.edu>
2021-09-12 00:05:44 -0400

    search mixin

--- app/models.py
+++ app/models.py
@@ -8,6 +8,52 @@ from . import db, login_manager
 import redis, rq, time, json, re, jwt
 from instance.config import *
 from time import time
+from app.search import add_to_index, remove_from_index, query_index
+
+
+class SearchableMixin(object):
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return cls.query.filter_by(id=0), 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        return (
+            cls.query.filter(cls.id.in_(ids)).order_by(db.case(when, value=cls.id)),
+            total,
+        )
+
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            "add": list(session.new),
+            "update": list(session.dirty),
+            "delete": list(session.deleted),
+        }
+
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes["add"]:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes["update"]:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes["delete"]:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+
+    @classmethod
+    def reindex(cls):
+        for obj in cls.query:
+            add_to_index(cls.__tablename__, obj)
+
+
+db.event.listen(db.session, "before_commit", SearchableMixin.before_commit)
+db.event.listen(db.session, "after_commit", SearchableMixin.after_commit)
 
 
 class Permission:
@@ -50,6 +96,7 @@ def slugify(s):
 
 class Tag(db.Model):
     __tablename__ = "tags"
+    __searchable__ = ["name, value"]
     id = db.Column(db.Integer, primary_key=True)
     term_id = db.Column(db.Integer, db.ForeignKey("terms.id"))
     name = db.Column(db.Text)
@@ -341,6 +388,7 @@ class Relationship(db.Model):
 
 class Term(db.Model):
     __tablename__ = "terms"
+    __searchable__ = ["term"]
     id = db.Column(db.Integer, primary_key=True)
     term = db.Column(db.String(64))
     definition = db.Column(db.Text)
@@ -460,6 +508,7 @@ class Track(db.Model):
 
 class Comment(db.Model):
     __tablename__ = "comments"
+    __searchable__ = ["body"]
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
     body_html = db.Column(db.Text)
